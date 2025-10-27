5b3de4c1b3be4413b91ed9f5c699895a2726cc06
HEMANATH-2005-K <hemanath.k.kumaresan@gmail.com>
2025-10-27 14:07:25 +0530

    Add files via upload

---
+++ Start_CraneIQ_App.bat
@@ -0,0 +1,26 @@
+@echo off
+title CraneIQ Flutter App Launcher
+color 0B
+echo.
+echo ========================================
+echo    CRANEIQ FLUTTER APP LAUNCHER
+echo ========================================
+echo.
+echo Starting CraneIQ Mobile App...
+echo Location: E:\PROJECTS\CraneIQ-master
+echo.
+echo This will run your Flutter app
+echo App will open in your default browser
+echo.
+echo ========================================
+echo.
+
+cd /d "E:\PROJECTS\CraneIQ-master"
+flutter run
+
+echo.
+echo ========================================
+echo    Flutter App Closed
+echo ========================================
+echo.
+pause
